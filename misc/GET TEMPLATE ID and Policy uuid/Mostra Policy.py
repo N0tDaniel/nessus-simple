8df3f86f456dc5397153ca8e619d8ad1567d9a87
NotDaniel <42096350+N0tDaniel@users.noreply.github.com>
2023-10-30 09:30:42 +0100

    Update Mostra Policy.py

--- misc/GET TEMPLATE ID and Policy uuid/Mostra Policy.py
+++ misc/GET TEMPLATE ID and Policy uuid/Mostra Policy.py
@@ -2,9 +2,9 @@ import requests
 import json
 from requests.exceptions import SSLError
 
-url = "https://172.25.196.32:8834/policies"
-access_key = "9d507eb6b90e1288ff7a9de6ebc54ccc7c339a813d55597c6bcbccd44409fc89"
-secret_key = "5e04b96e748e6f6b5c1c2b8df5231f7b67d9f323fadec19beebcef9897f2cd88"
+url = "https://ip:8834/policies"
+access_key = ""
+secret_key = ""
 
 headers = {
     "accept": "application/json",
